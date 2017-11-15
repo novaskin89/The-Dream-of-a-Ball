5e54d3014af7da3966e0d8ca507de11758e49daf
DavideMC <novaskin89@gmail.com>
2017-11-15 20:33:51 +0100

    Aggiunto 2 script riguardanti un esercizio per gli enum

---
+++ The Dream of a Ball/Assets/Scripts/EnumExample.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumExample : MonoBehaviour
+{
+    public enum EnemyState { Searching, Attacking, Dead }
+    public EnemyState currentEnemyState;
+    // Use this for initialization
+    void Start()
+    {
+        currentEnemyState = EnemyState.Searching;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentEnemyState)
+        {
+            case EnemyState.Attacking:
+                Debug.Log("Attacking");
+                break;
+
+            case EnemyState.Searching:
+                Debug.Log("Searching");
+                break;
+
+            case EnemyState.Dead:
+                Debug.Log("Dead");
+                break;
+
+
+        }
+    }
+}
---
+++ The Dream of a Ball/Assets/Scripts/InputScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputScript : MonoBehaviour
+{
+    private EnumExample enumExample;
+
+    // Use this for initialization
+    void Awake()
+    {
+        enumExample = GetComponent<EnumExample>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            enumExample.currentEnemyState = (EnumExample.EnemyState)Random.Range(0, 3);
+        }
+    }
+}
